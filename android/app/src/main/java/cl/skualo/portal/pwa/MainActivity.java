df1ed7eb8e821007512f256598f0b7b60f4f851a
Valeria Gigena <valeria@skualo.cl>
2024-05-25 11:09:23 -0300

    android apk GoogleAuth

--- android/app/src/main/java/cl/skualo/portal/pwa/MainActivity.java
+++ android/app/src/main/java/cl/skualo/portal/pwa/MainActivity.java
@@ -1,13 +1,5 @@
 package cl.skualo.portal.pwa;
-import android.os.Bundle;
-import com.epicshaggy.biometric.NativeBiometric;
+
 import com.getcapacitor.BridgeActivity;
-import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
-public class MainActivity extends BridgeActivity {
-    @Override
-     public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(NativeBiometric.class);
-        registerPlugin(GoogleAuth.class);
-        super.onCreate(savedInstanceState);
-     }
-}
+
+public class MainActivity extends BridgeActivity {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,15 +14,6 @@ export const environment = {
     scope: 'https://graph.microsoft.com/User.Read',
     authorizationBaseUrl: `https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize`,
     accessTokenEndpoint: 'https://login.microsoftonline.com/consumers/oauth2/v2.0/token',
-  },
-  firebase: {
-      apiKey: "AIzaSyBXw19TsA7tEfmYGy1BQyI1iZehHw3TCTQ",
-      authDomain: "portal-trabajadores-423220.firebaseapp.com",
-      projectId: "portal-trabajadores-423220",
-      storageBucket: "portal-trabajadores-423220.appspot.com",
-      messagingSenderId: "1005811561165",
-      appId: "1:1005811561165:web:c371cc53cfb329dc4d2f84",
-      measurementId: "G-D1PRM6S2PX"
-    }
+  }
   }
 
